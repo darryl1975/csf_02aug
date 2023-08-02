b8eae6c086dc57c52077a77f8304f9efeaea75a5
Darryl <darryl1975@hotmail.com>
2023-08-02 15:23:44 +0800

    completed post create

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,20 +7,29 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './fruits/home/home.component';
 import { CreateComponent } from './fruits/create/create.component';
 import { EditComponent } from './fruits/edit/edit.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { PostListComponent } from './posts/post-list/post-list.component';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { PostEditComponent } from './posts/post-edit/post-edit.component';
+import { PostDetailsComponent } from './posts/post-details/post-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     CreateComponent,
-    EditComponent
+    EditComponent,
+    PostListComponent,
+    PostCreateComponent,
+    PostEditComponent,
+    PostDetailsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { Post } from '../post';
+import { PostService } from '../post.service';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent {
+  posts: Post[] = [];
+
+  constructor(private postService: PostService) { }
+
+  ngOnInit() : void {
+    this.postService.getAll().subscribe((data : Post[]) => {
+      this.posts = data;
+    })
+  }
+}
